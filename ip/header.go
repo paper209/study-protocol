1291358fb7916d321538a3c445af0fa70445cdb7
paper209 <fleshed-stroll84@icloud.com>
2026-03-26 06:54:30 +0900

    Update header.go

--- ip/header.go
+++ ip/header.go
@@ -1,6 +1,10 @@
 package ip
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"errors"
+	"study/checksum"
+)
 
 // total bytes = 20
 type Header struct {
@@ -25,9 +29,33 @@ func (h *Header) BuildHeader() []byte {
 	binary.BigEndian.PutUint16(buf[6:], h.FlagsFragment)
 	buf[8] = h.TTL
 	buf[9] = h.Protocol
-	binary.BigEndian.PutUint16(buf[10:], h.getChecksum())
+	binary.BigEndian.PutUint16(buf[10:], 0) // checksum
 	copy(buf[12:16], h.SourceAddress[:])
 	copy(buf[16:20], h.DestinationAddress[:])
 
+	// real checksum
+	binary.BigEndian.PutUint16(buf[10:], checksum.Checksum(buf))
+
 	return buf
+}
+
+func DecodeHeader(buf []byte) (*Header, error) {
+	if len(buf) < 20 {
+		return nil, errors.New("Invalid Length")
+	} else if checksum.Checksum(buf[:20]) != 0 {
+		return nil, errors.New("Invalid Checksum")
+	}
+
+	return &Header{
+		VersionIHL:         buf[0],
+		Tos:                buf[1],
+		TotalLength:        binary.BigEndian.Uint16(buf[2:4]),
+		Identification:     binary.BigEndian.Uint16(buf[4:6]),
+		FlagsFragment:      binary.BigEndian.Uint16(buf[6:8]),
+		TTL:                buf[8],
+		Protocol:           buf[9],
+		Checksum:           binary.BigEndian.Uint16(buf[10:12]),
+		SourceAddress:      [4]byte{buf[12], buf[13], buf[14], buf[15]},
+		DestinationAddress: [4]byte{buf[16], buf[17], buf[18], buf[19]},
+	}, nil
 }
